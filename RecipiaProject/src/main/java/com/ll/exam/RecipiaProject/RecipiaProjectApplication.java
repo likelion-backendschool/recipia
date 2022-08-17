6b7ca5baaf88eefa69f53ebc032cb360490d8e82
jihun-24k <gi020477@gmail.com>
2022-08-17 09:21:11 +0900

    Chore: 기본 스프링부트 프로젝트 생성

---
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/RecipiaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.RecipiaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipiaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipiaProjectApplication.class, args);
+	}
+
+}
---
+++ RecipiaProject/src/test/java/com/ll/exam/RecipiaProject/RecipiaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.RecipiaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipiaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
