bd23e3a1287a468376e13ca373e40dcf3ac498da
jihun-24k <gi020477@gmail.com>
2022-08-21 14:42:12 +0900

    Feat: PasswordEncoder Bean으로 등록

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/user/UserService.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/user/UserService.java
@@ -1,6 +1,7 @@
 package com.ll.exam.RecipiaProject.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,11 +9,12 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void create(String username, String password, String email){
         SiteUser newUser = SiteUser.builder()
                 .username(username)
-                .passward(password)
+                .passward(passwordEncoder.encode(password))
                 .email(email)
                 .build();
 
