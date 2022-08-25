93d82f50f03b7b76af95873f4034749df1b6eae6
SangHun <shzero211@naver.com>
2022-08-26 03:44:26 +0900

    feat:PostImg 에  NoArgs,Builder 추가

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/post/postImage/PostImg.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/post/postImage/PostImg.java
@@ -1,13 +1,16 @@
 package com.ll.exam.RecipiaProject.post.postImage;
 
 import com.ll.exam.RecipiaProject.post.Post;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 public class PostImg {
     @Id
@@ -20,9 +23,16 @@ public class PostImg {
 
     private String imgUrl;
 
-    private boolean ThumbnailYn;
+    private boolean thumbnailYn;
 
     @ManyToOne
     private Post post;
 
+    @Builder
+    public PostImg(String imgName,String oriImgName,String imgUrl,boolean thumbnailYn){
+        this.imgName=imgName;
+        this.oriImgName=oriImgName;
+        this.imgUrl=imgUrl;
+        this.thumbnailYn=thumbnailYn;
+    }
 }
