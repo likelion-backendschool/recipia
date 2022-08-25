8e937512c13be4831c965d712b6922607e797a36
SangHun <shzero211@naver.com>
2022-08-25 18:00:37 +0900

    feat:PostController 메소드 주석 추가

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/post/PostController.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/post/PostController.java
@@ -14,17 +14,20 @@ public class PostController {
 
     private final PostService postService;
 
+    //게시글 작성 폼으로 이동
     @GetMapping("")
     public String postForm(Model model){
 
         return "post/postForm";
     }
 
+    //게시글 작성
     @PostMapping("")
     public String postCreate(){
         return "redirect:/posts/list";
     }
 
+    //게시글 리스트로 이동
     @GetMapping("/list")
     public String posts(Model model){
         List<Post> posts=postService.getPostList();
@@ -32,19 +35,25 @@ public class PostController {
         return "post/postList";
     }
 
+    //게시글 상세 페이지로 이동
     @GetMapping("/{postId}")
     public String postDetail(@PathVariable("postId") int postId,Model model){
         return "post/postDetail";
     }
 
+    //게시글 수정 페이지로 이동
     @GetMapping("/{postId}/modify")
     public String postModifyForm(@PathVariable("postId") int postId,Model model){
         return "post/postModifyForm";
     }
+
+    //게시글 수정
     @PatchMapping("/{postId}/modify")
     public String postModify(@PathVariable("postId") int postId){
         return "redirect:/posts/list";
     }
+
+    //게시글 삭제
     @DeleteMapping("/{postId}")
     public String postDelete(@PathVariable("postId") int postId){
         return "redirect:/posts/list";
