bfa8a94559d385f0b3b30b3bd16d1245d316aa09
SG1515 <sunggu507@naver.com>
2022-08-25 17:53:32 +0900

    Feat: MyPage 엔티티 작성

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/mypage/MyPage.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/mypage/MyPage.java
@@ -1,4 +1,23 @@
 package com.ll.exam.RecipiaProject.mypage;
 
-public class myPage {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class MyPage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String passward;
 }
