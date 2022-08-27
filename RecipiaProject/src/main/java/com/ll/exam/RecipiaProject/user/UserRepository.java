8943fb2748d935c223ddbded99a3c91d502660e1
jihun-24k <gi020477@gmail.com>
2022-08-27 10:59:49 +0900

    Feat: UserRole 추가, username으로 DB 조회

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/user/UserRepository.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/user/UserRepository.java
@@ -2,7 +2,11 @@ package com.ll.exam.RecipiaProject.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
+
+    Optional<SiteUser> findByusername(String username);
 }
---
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/user/UserRole.java
@@ -0,0 +1,16 @@
+package com.ll.exam.RecipiaProject.user;
+
+import lombok.Getter;
+
+
+@Getter
+public enum UserRole {
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+
+    UserRole(String value) {
+        this.value = value;
+    }
+
+    private String value;
+}
