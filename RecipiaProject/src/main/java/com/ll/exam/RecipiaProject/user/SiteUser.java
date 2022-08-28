4394db82f069cb3c2618cdd9309028d88d51d053
SangHun <shzero211@naver.com>
2022-08-28 16:30:40 +0900

    feat:SiteUser 에 No,All 생성자 추가

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/user/SiteUser.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/user/SiteUser.java
@@ -1,13 +1,17 @@
 package com.ll.exam.RecipiaProject.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SiteUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
