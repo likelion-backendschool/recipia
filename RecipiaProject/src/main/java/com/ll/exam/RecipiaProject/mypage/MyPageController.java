92e86c065e0dbf58740aa884512ed59c0cdd1a3e
SG1515 <sunggu507@naver.com>
2022-09-02 02:51:32 +0900

    Feat: MyPageController Optional 추가(추후 보완)

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/mypage/MyPageController.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/mypage/MyPageController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional; //NullpointerException 해결
+
 @RequestMapping("/mypage") // 여기에 적으면 MyPageController URL 상위
 @Controller
 @RequiredArgsConstructor // 생성자 주입.
@@ -16,7 +18,7 @@ public class MyPageController {
 
     @GetMapping("/home")
     public String mypageHome() {
-        SiteUser siteUser = userRepository.findByUsername("user1")
+        Optional<SiteUser> siteUser = userRepository.findByUsername("user1");
 
         return "mypage/mypage_home";
     }
