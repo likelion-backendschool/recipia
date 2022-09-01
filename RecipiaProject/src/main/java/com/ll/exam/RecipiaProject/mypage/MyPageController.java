052e894b8dc214bbd3b9c29eb50373295faa3d98
SG1515 <sunggu507@naver.com>
2022-09-02 00:41:45 +0900

    Feat: MyPageController 생성/생성자 주입/GetMapping

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/mypage/MyPageController.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/mypage/MyPageController.java
@@ -1,5 +1,21 @@
 package com.ll.exam.RecipiaProject.mypage;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/mypage") // MyPageController 안에 매핑되는 상위 page.
+@Controller
+@RequiredArgsConstructor // 생성자 주입.
 public class MyPageController {
+
+
+    @GetMapping("/home")
+    public String mypageHome() {
+
+        return "mypage_home";
+    }
+
 
 }
