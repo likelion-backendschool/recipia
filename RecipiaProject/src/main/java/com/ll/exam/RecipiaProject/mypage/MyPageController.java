8b0a9cafe032bdf47ed442a9fa9e2a87420c29a8
SG1515 <sunggu507@naver.com>
2022-09-02 00:53:15 +0900

    Feat: UserRepository 필드 주입

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/mypage/MyPageController.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/mypage/MyPageController.java
@@ -1,15 +1,17 @@
 package com.ll.exam.RecipiaProject.mypage;
 
+import com.ll.exam.RecipiaProject.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/mypage") // MyPageController 안에 매핑되는 상위 page.
+@RequestMapping("/mypage") // 여기에 적으면 MyPageController URL 상위
 @Controller
 @RequiredArgsConstructor // 생성자 주입.
 public class MyPageController {
 
+    private final UserRepository userRepository;
 
     @GetMapping("/home")
     public String mypageHome() {
