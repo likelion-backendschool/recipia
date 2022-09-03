9383c0a202189b484df468a4e0bcedb99be60b37
jinsu291 <wlstn291@naver.com>
2022-09-03 22:59:16 +0900

    feat: 검색창 추가

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/home/HomeController.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/home/HomeController.java
@@ -1,6 +1,5 @@
 package com.ll.exam.RecipiaProject.home;
 
-import com.ll.exam.RecipiaProject.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
