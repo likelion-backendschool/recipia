dee508a579ec76c61658daabc9e2fe6b01bf0d19
SangHun <shzero211@naver.com>
2022-09-04 23:37:18 +0900

    feat:PostFormDto-> imgDto id 저장할 리스트 추가

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/post/PostFormDto.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/post/PostFormDto.java
@@ -20,8 +20,10 @@ private String title;
 
 private String content;
 
-//상세페이지 구현시 이미지 전송 에 사용될 변수
+//작성폼 수정시 사용
 private List<PostImgDto> postImgDtoList=new ArrayList<>();
+//작성폼 수정시 사용
+private List<Integer>postImgIds =new ArrayList<>();
 
 public Post createPost(SiteUser siteUser){
     return Post.builder()
