ece120d6b41cfe346c50e91fa236a2c7076883b8
SG1515 <sunggu507@naver.com>
2022-09-13 03:12:11 +0900

    Feat: 삭제를 구현하기 위해 Service 생성

---
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/mypage/MyPageService.java
@@ -0,0 +1,18 @@
+package com.ll.exam.RecipiaProject.mypage;
+
+import com.ll.exam.RecipiaProject.user.SiteUser;
+import com.ll.exam.RecipiaProject.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyPageService {
+
+    private final UserRepository userRepository;
+
+
+    public void delete(SiteUser siteUser) {
+        this.userRepository.delete(siteUser);
+    }
+}
