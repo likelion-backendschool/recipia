92636841e14dfa42b9260aa4e72d01e5451bf3a5
SG1515 <sunggu507@naver.com>
2022-09-13 19:06:48 +0900

    Feat: 6주차 위클리 작성

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/mypage/DataNotFoundException.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/mypage/DataNotFoundException.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "데이터가 존재하지 않습니다.")
 public class DataNotFoundException extends RuntimeException {
-    public DataNotFoundException(String question_not_found) {
+    public DataNotFoundException(String id_not_found) {
     }
 }
