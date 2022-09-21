780d50c5dc7859bf9537459301bd3748f8c13b19
jihun-24k <gi020477@gmail.com>
2022-09-21 16:31:08 +0900

    Feat: Ajax 코드 추가

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/comment/controller/CommentController.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/comment/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.ll.exam.RecipiaProject.comment.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +12,11 @@ public class CommentController {
     @GetMapping("")
     public String showComment(){
         return "comment/comment";
+    }
+
+    @PostMapping("")
+    public String postComment(){
+        return "redirect:/comment";
     }
 
 }
