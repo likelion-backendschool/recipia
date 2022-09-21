23d12401fcb7c1a85ffe1fd0086183953a6df0be
jihun-24k <gi020477@gmail.com>
2022-09-21 16:33:21 +0900

    Feat: CommentDto 추가

---
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/comment/dto/CommentDto.java
@@ -0,0 +1,11 @@
+package com.ll.exam.RecipiaProject.comment.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentDto {
+    private String msg;
+    private String result;
+}
