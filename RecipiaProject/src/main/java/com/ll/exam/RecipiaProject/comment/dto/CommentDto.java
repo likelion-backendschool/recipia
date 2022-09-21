d5db0f0fad5a803cd21f1a86cede9cd44817338e
jihun-24k <gi020477@gmail.com>
2022-09-22 00:58:25 +0900

    Feat: CommentDto 수정

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/comment/dto/CommentDto.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/comment/dto/CommentDto.java
@@ -1,8 +1,14 @@
 package com.ll.exam.RecipiaProject.comment.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommentDto {
-    private String result;
+    private int userId;
+    private int postId;
+    private String content;
 }
