6d5f3ed1fdcffff51ad567893fdf29e6fd115623
SangHun <shzero211@naver.com>
2022-09-23 23:45:04 +0900

    refactor:createdDate,modifiedDate 를 담은 BaseTime 생성

--- RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/RecipiaProjectApplication.java
+++ RecipiaProject/src/main/java/com/ll/exam/RecipiaProject/RecipiaProjectApplication.java
@@ -2,8 +2,10 @@ package com.ll.exam.RecipiaProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RecipiaProjectApplication {
 
 	public static void main(String[] args) {
